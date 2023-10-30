e89bff37308babbb8ac81f00a63cca5a51653a8f
Hana Zaraa <hana.zaraa@riadvice.tn>
2023-10-30 12:09:00 +0100

    updates

--- puppy.go
+++ puppy.go
@@ -1,8 +1,18 @@
 package puppy
 
+import (
+	"github.com/hanazarraa/dog"
+)
+
 func Bark() string {
 	return "woof"
 }
 func Barks() string {
 	return "woof woof woof"
+}
+func BigBark() string {
+	return dog.WhenGrowUp(Bark())
+}
+func BigBarks() string {
+	return dog.WhenGrowUp(Barks())
 }
